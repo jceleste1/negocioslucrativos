6dec45492c0b0635bf68976267505e33b55c8ece
xb201250 <jsoares@prservicos.com.br>
2017-11-08 15:38:58 -0200

    migrando....

--- mvcAnnouncement.php
+++ mvcAnnouncement.php
@@ -172,10 +172,10 @@
 								 a.viewcount,a.confidencial,r.name,r.phonemobile,r.phone,
 								 a.price,a.www  from announcement a, register r 
 								 where  a.id_user =r.id and    a.id='%s'", mysqli_real_escape_string($conn,$_REQUEST["id_adv"]) );				
-			echo $qry;  break;
+			
 				$result =  $conn->query( $qry );
 				$dataAnnouncement = $result->fetch_assoc() ;
-
+echo $qry;  break;
 				$viewcount = ( $dataAnnouncement["viewcount"] +1 ) ;
 				echo "parou...............";
 break;
