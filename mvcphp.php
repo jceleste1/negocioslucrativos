5af5568c3572d8846ff224d8f4b15882bf994a23
xb201250 <jsoares@prservicos.com.br>
2017-11-10 17:14:04 -0200

    migrando..

--- mvcphp.php
+++ mvcphp.php
@@ -1,14 +1,3 @@
 <? 
-	session_start();
-
-
-
-	header('Content-Type: text/html; charset=ISO-8859-1');
-
-
-	include("config.inc");
-	echo "processando..";
-
-	include("nlTemplate.php");
-
+echo "processando....";
 ?>
