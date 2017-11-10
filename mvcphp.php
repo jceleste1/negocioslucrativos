218370c0855846f14b3b704327c8291e62f670df
xb201250 <jsoares@prservicos.com.br>
2017-11-10 17:16:08 -0200

    migrando..

--- mvcphp.php
+++ mvcphp.php
@@ -1,3 +1,14 @@
 <? 
-echo "processando....";
+	session_start();
+
+
+
+	header('Content-Type: text/html; charset=ISO-8859-1');
+
+
+	include("config.inc");
+	 $conn = connectDB();
+
+	 $action = "";
+	 echo "processando .......";
 ?>
